f32d04d9011c065691238bebd1bd0f810b837394
u31999 <u31999@gmail.com>
2022-06-23 16:26:53 -0700

    Add leaflet map

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -5,7 +5,7 @@ import {getAuth} from 'firebase/auth'
 import { db } from "../firebase.config"
 import Spinners from "../component/Spinners"
 import shareIcon from '../assets/svg/shareIcon.svg'
-import { async } from "@firebase/util"
+import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet'
 
 function Listing() {
     const [listing, setListing] = useState(null)
@@ -77,7 +77,18 @@ function Listing() {
         </ul>
 
         <p className="listingLocationTitle">Location</p>
-        {/*Map */}
+        <div className="leafletContainer">
+            <MapContainer style={{height: '100%', width: '100%'}}
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13} scrollWheelZoom={false}>
+                <TileLayer 
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png' />
+                <Marker position={[listing.geolocation.lat, listing.geolocation.lng]}>
+                    <Popup>{listing.location}</Popup>
+                </Marker>
+            </MapContainer>
+        </div>
 
         {auth.currentUser?.uid !== listing.userRef && (
             <Link to={`/contact/${listing.userRef}?listingName=${listing.name}`}
